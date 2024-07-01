fcdbd768ea1a2e52401270e3c88ff51cf9cb47f8
Andecochea <andecomaf@gmail.com>
2024-07-01 11:57:55 +0200

    Commit final

--- entregas/Examen Extraordinario/Cotillas.java
+++ entregas/Examen Extraordinario/Cotillas.java
@@ -1,24 +1,42 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
-public class Cotillas{
-    public static void main (String[]args){
+public class FiestaDeCotillas {
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         System.out.print("Ingrese el número de invitados que acuden a la fiesta : ");
         int numeroPersonas = scanner.nextInt();
-
+        
+        
         if (numeroPersonas <= 4) {
             System.out.println("El número de invitados debe ser mayor a 4 para que el rumor pueda propagarse.");
             return;
         }
-
         
         int[] vecesEscuchado = new int[numeroPersonas];
+        
         int personaActual = random.nextInt(numeroPersonas);
         vecesEscuchado[personaActual]++;
-
+        
         System.out.println("El rumor empieza en la persona: " + personaActual);
+        
+        while (true) {
+            int nuevaPersona;
+            do {
+                nuevaPersona = random.nextInt(numeroPersonas);
+            } while (nuevaPersona == personaActual || vecesEscuchado[nuevaPersona] >= 2);
+
+            System.out.println("La persona " + personaActual + " le cuenta el rumor a la persona " + nuevaPersona);
+            vecesEscuchado[nuevaPersona]++;
+            if (vecesEscuchado[nuevaPersona] == 2) {
+                System.out.println("El rumor se detiene porque la persona " + nuevaPersona + " ya había escuchado el rumor dos veces.");
+                break;
+            }
+
+            personaActual = nuevaPersona;
+        }
+        
     }
-
-
 }
