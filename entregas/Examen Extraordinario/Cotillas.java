ed760f096ebef8b771b353a6e550afe76cb5a3ec
Andecochea <andecomaf@gmail.com>
2024-07-01 11:27:08 +0200

    Numro de participantes

--- entregas/Examen Extraordinario/Cotillas.java
+++ entregas/Examen Extraordinario/Cotillas.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Cotillas{
     public static void main (String[]args){
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el número de invitados que acuden a la fiesta : ");
+        int numeroPersonas = scanner.nextInt();
+
+        if (numeroPersonas <= 4) {
+            System.out.println("El número de invitados debe ser mayor a 4 para que el rumor pueda propagarse.");
+            return;
+        }
     }
 }
